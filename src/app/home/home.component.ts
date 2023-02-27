a1de804ccc0295d6faee38f27d2d086d276edd8a
Jonathan Velasco <jvelasco202300050@team.acquirebpo.com>
2023-02-27 18:35:15 +1100

    add home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, Output, EventEmitter, Input } from "@angular/core";
+@Component({
+	selector: "home",
+	templateUrl: "./home.component.html",
+	styleUrls: ["./home.component.scss"],
+})
+export class Home implements OnInit {
+    constructor(){
+
+    }
+    ngOnInit() {}
+}
